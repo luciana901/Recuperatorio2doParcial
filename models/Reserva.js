197e8bbfa4af308927c3d7cfd49581e7a1d71465
Lezcano Luciana <lezcanoceleste1972@gmail.com>
2023-07-05 15:12:25 -0300

    Cree el modelado de datos

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,3 +1,65 @@
 // TODO: Crear modelo de datos de Reserva
 const { Sequelize } = require("sequelize");
 const { Sequelize, DataTypes } = require("sequelize");
+
+const Reserva = sequelize.define(
+  "Reserva",
+  {
+    // aca tengo que poner la informacion de los datos en la base de dato, su tipo y eso nomas creo
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    codigo: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    fecha_vuelo: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updateAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    deleteAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+  },
+  {
+    createAt: true,
+    updateAt: true,
+    deleteAt: true,
+    tableName: "reservas",
+  }
+);
