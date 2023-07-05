3644cc6bfebedfd2cc35960ad529023cbbbe5d6b
Lezcano Luciana <lezcanoceleste1972@gmail.com>
2023-07-05 14:50:50 -0300

    Complete el código faltante del archivo de arranque del servidor (app.js)

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Imports
 const cors = require("cors");
 const express = require("express");
-const helmet = require("helmet");
 const path = require("path");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 require("dotenv").config();
+
+require("ejs");
 
 const app = express();
 
@@ -12,6 +15,12 @@ const port = process.env.PORT || 5000;
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -19,6 +28,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+  return res.status(404).render("404");
+});
 
 // Starting the server
 app.listen(port, () => console.log("Server on http://localhost:${port}"));
