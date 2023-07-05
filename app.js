78ffe7f490bafee6fcc51572a2cc44b882b4fc1f
Lezcano Luciana <lezcanoceleste1972@gmail.com>
2023-07-05 14:44:32 -0300

    Cree el archivo README.md y coloque en él, los pasos para instalar y ejecutar el proyecto creado durante el examen.

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
 
-const path = require('path');
+require("dotenv").config();
 
 const app = express();
+
+const port = process.env.PORT || 5000;
 
 // Middlewares
 // TODO: Implementar middlewares
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use("/api", require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log("Server on http://localhost:${port}"));
